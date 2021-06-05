5b1328c0dee54460fde9aaf1600494afaf2b8de3
caglaoflazoglu <82257381+caglaoflazoglu@users.noreply.github.com>
2021-06-05 10:54:34 +0300

    Create question6.php

---
+++ question6.php
@@ -0,0 +1,6 @@
+$data = $request->all()
+$student = New Student () ;
+$student ->name=$data['name'];
+$student ->surname=$data['surname'];
+$student ->department=$data['department'];
+$student ->id=$data['id'];
